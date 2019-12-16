ad245d24db9f84f94a6fb97f56468ad3e7e29d8e
Richard B. Porter <rbp@richardbporter.com>
2019-12-16 13:16:00 -0600

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
+import "./styles/global.css"
+import rbp from "../img/rbp.jpg"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div class="font-sans max-w-lg bg-gray-100 mt-20 mx-auto my-auto shadow-lg rounded-lg overflow-hidden">
+        <div class="sm:flex sm:items-center px-6 py-4">
+            <img class="block mx-auto sm:mx-0 sm:flex-shrink-0 h-16 sm:h-24 rounded-full" src={rbp} alt="richardbporter" />
+            <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
+            <p class="text-xl leading-normal">richardbporter</p>
+            <p class="text-sm leading-normal text-gray-600">Drupal Developer</p>
+            <p>Hi! I'm a Drupal developer from North Liberty, IA. For the past 10 years, I've been at The University of Iowa doing all things Drupal with a little DevOps mixed is as well. Get a hold of me on various social platforms. Cheers!</p>
+
+            <div class="mt-4 flex">
+                <a class="flex-auto text-pink-500 hover:text-pink-700" href="https://github.com/richardbporter">GitHub</a>
+                <a class="flex-auto text-pink-500 hover:text-pink-700" href="https://drupal.org/u/rbp">Drupal</a>
+                <a class="flex-auto text-pink-500 hover:text-pink-700" href="https://twitter.com/richardbporter">Twitter</a>
+            </div>
+            </div>
+        </div>
+    </div>
+)
